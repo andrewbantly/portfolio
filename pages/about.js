a471f709f8ac7b10e926a52630528d81f64ca115
andrewbantly <andrewbantly@gmail.com>
2023-05-29 11:18:15 -0700

    updated about page copy

--- pages/about.js
+++ pages/about.js
@@ -34,7 +34,13 @@ export default function About() {
                 </div>
             {/* </div> */}
             <button onClick={() => setFontSize(fontSize + 0.5)} className={`${styles.fontSizeButton}`}>Enlarge font</button>
-            <p className={`biggerText ${styles.biggerText}`}>I am a software engineer who is building his portfolio. 👨🏻‍💻</p>
+            <p className={`biggerText ${styles.biggerText}`}>Hi there, I'm Andrew.</p>
+            <p className={`biggerText ${styles.biggerText}`}>I'm a highly motivated software engineer with six years of experience in media production. My passion lies in finding innovative solutions that not only meet expectations but exceed them. I excel in creative problem solving, project management, and effective communication.</p>
+            <p className={`biggerText ${styles.biggerText}`}>Recently, I completed an immersive software engineering bootcamp with General Assembly. Over the course of 12 weeks, I dedicated more than 600 hours to learning various technologies, including Git, Github, HTML, CSS, JavaScript, Node, MongoDB, PostgreSQL, React, Next, Python, and more.</p>
+            <p className={`biggerText ${styles.biggerText}`}>I take pride in being an alumnus of the University of Oregon, where I honed my storytelling skills. Following graduation, I embarked on a solo journey to the Dominican Republic, producing a documentary that marked the beginning of my career.</p>
+            <p className={`biggerText ${styles.biggerText}`}>Throughout my professional journey, I've had the privilege of collaborating with diverse organizations. I've contributed to Reliable Robotics, a startup focused on automating aircraft operations, as well as the San Francisco Giants, a professional baseball organization. Additionally, I've worked with the CK-12 Foundation, a non-profit organization dedicated to providing free, high-quality education.</p>
+            <p className={`biggerText ${styles.biggerText}`}>When I'm not coding, you can find me pursuing various outdoor activities. I enjoy skiing down challenging slopes, biking through scenic roadways, and taking leisurely walks with my dog Murphy. As a sports enthusiast, I find inspiration in teamwork, competition, and the expression: “pressure is a privilege.”</p>
+            <p className={`biggerText ${styles.biggerText}`}>When I'm not coding, you can find me engaging in various outdoor activities. I enjoy skiing down challenging slopes, exploring scenic roadways on my bike, and enjoying leisurely walks with my dog, Murphy. As a sports enthusiast, I draw inspiration from teamwork, competition, and the mantra: "pressure is a privilege."</p>
             {/* local images are stored in public folder */}
             {/* regular <img/> tags work as well, if you cannot provide a width and height */}
             {/* using another hosting domain for images */}
