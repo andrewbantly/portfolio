4a5bd62b291ab57733ecb78d3eab669a2d8fa746
andrewbantly <andrewbantly@gmail.com>
2023-05-31 15:09:18 -0700

    updated project data

--- projectData.js
+++ projectData.js
@@ -5,15 +5,15 @@ const myProjects = [
         description: "An immersive data visualization application that merges D3.js (Data-Driven Documents), React, and Python.",
         tools: ["JavaScript", "D3.js", "React", "Python", "CSS"]
     }, {
-        name: "Node application",
-        link: "https://pickem.herokuapp.com/",
-        description: "A full-stack Node application using modern JavaScript coding and debugging techniques that incorporates four relational database management system models, user authentication and authorization, RESTful APIs, and object-relational mapping (ORM).",
-        tools: ["Full-stack", "JavaScript", "Node.js","PostgreSQL", "Sequelize", "EJS", "Express", "bcrypt", "crypto-js", "CSS"]
-    }, {
         name: "MERN stack application",
         link: "https://nps-explorer.netlify.app/",
         description: "Worked in a small team to develop an interactive, user-experience driven MERN stack application with RESTful JSON APIs, integrated third-party APIs, CRUD functionalities, and managed Git merges and resolved merge conflicts.",
         tools: ["Full-stack", "JavaScript", "CSS", "React", "Node.js","MongoDB", "Mongoose", "Express", "bcryptjs", "JSON Web Token"]
+    }, {
+        name: "Node application",
+        link: "https://pickem.herokuapp.com/",
+        description: "A full-stack Node application using modern JavaScript coding and debugging techniques that incorporates four relational database management system models, user authentication and authorization, RESTful APIs, and object-relational mapping (ORM).",
+        tools: ["Full-stack", "JavaScript", "Node.js","PostgreSQL", "Sequelize", "EJS", "Express", "bcrypt", "crypto-js", "CSS"]
     }, {
         name: "Game",
         link: "https://andrewbantly.github.io/Escape-From-Alcatraz/",
