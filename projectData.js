fb67953610bce0221988d800bc288177611ba2ae
andrewbantly <andrewbantly@gmail.com>
2023-10-30 18:42:33 -0700

    updated projects

--- projectData.js
+++ projectData.js
@@ -1,23 +1,33 @@
 const myProjects = [
     {
-        name: "Data visualization",
+        name: "LeasePeek (in progress)",
+        link: "https://github.com/andrewbantly/leasepeek",
+        description: "Building a data-driven web app for large-scale property managers to forecast rent trends using analytics. Utilizing TypeScript, React, Chakra UI, Python, Django, Pandas, MongoDB, PostgreSQL, and REST APIs. Scripting unit and integration tests; leveraging Docker and DevOps for robust, scalable deployments.",
+        tools: ["TypeScript", "React", "Chakra UI", "Python", "Django", "Pandas", "MongoDB", "PostgreSQL", "REST APIs", "JSON Web Token"]
+    }, {
+        name: "GroundBreakers Excavation",
+        link: "https://groundbreakersexcavation.com/",
+        description: "Built a family business website with SEO and accessibility using JavaScript, React, Chakra UI, and Netlify. Integrated serverless email alerts, boosting visibility, inclusivity, and client engagement.",
+        tools: ["JavaScript", "React", "Chakra UI", "Netlify"]
+    }, {
+        name: "World Class Wines",
         link: "https://andrewbantly.github.io/world-class-wines/",
-        description: "Front end web development of a data visualization that merges Scalable Vector Graphics, JavaScript, D3.js, React.js, and Python for data structuring.",
+        description: "Programmed an interactive data visualization with JavaScript, React, D3.js, and Python, simplifying wine discovery by preferences, praised for user experience.",
         tools: ["JavaScript", "D3.js", "React", "Python", "CSS"]
     }, {
-        name: "MERN stack application",
+        name: "National Parks Explorer",
         link: "https://nps-explorer.netlify.app/",
-        description: "Worked in a small team to develop an interactive, user-experience driven MERN stack application with RESTful JSON APIs, integrated third-party APIs, CRUD functionalities, and managed Git merges and resolved merge conflicts.",
-        tools: ["Full-stack", "JavaScript", "CSS", "React", "Node.js","MongoDB", "Mongoose", "Express", "bcryptjs", "JSON Web Token"]
+        description: "Co-developed a MERN stack app, enabling users to discover U.S. national parks and log visits, with features like REST APIs, JWT authentication, and CRUD operations.",
+        tools: ["Full-stack", "JavaScript", "CSS", "React", "Node.js","MongoDB", "Mongoose", "Express", "bcryptjs", "JSON Web Token", "REST APIs"]
     }, {
-        name: "Node application",
+        name: "Major League Baseball Pick'em",
         link: "https://pickem.herokuapp.com/",
-        description: "Full stack development of a Node application using modern JavaScript coding and debugging techniques that incorporates four relational database management system models, RESTful APIs, and object-relational mapping.",
+        description: "Crafted a full-stack Node app using JavaScript, featuring real-time game odds, social interactions, and performance metrics, supported by relational databases.",
         tools: ["Full-stack", "JavaScript", "Node.js","PostgreSQL", "Sequelize", "EJS", "Express", "bcrypt", "crypto-js", "CSS"]
     }, {
-        name: "Game",
+        name: "Escape From Alcatraz",
         link: "https://andrewbantly.github.io/Escape-From-Alcatraz/",
-        description: "Front end development of a user-friendly game that incorporates document object model manipulation, behavioral logic, and object detection using JavaScript.",
+        description: "Engineered an interactive game leveraging JavaScript for DOM manipulation, behavioral logic, and object detection, enhanced with intricate CSS styling.",
         tools: ["JavaScript", "CSS", "HTML"]
     }
 ]
