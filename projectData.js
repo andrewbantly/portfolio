2b6f6ebedae6d74eb3cffb86d5d799fd257390dc
andrewbantly <andrewbantly@gmail.com>
2023-11-02 14:51:23 -0700

    added project description for LeasePeek

--- projectData.js
+++ projectData.js
@@ -3,7 +3,7 @@ const myProjects = [
         name: "LeasePeek (in progress)",
         link: "https://github.com/andrewbantly/leasepeek",
         description: "Engineering a data-driven app for large-scale property managers to forecast rent trends using analytics. Utilizing TypeScript, React, Chakra UI, Python, Django, Pandas, MongoDB, PostgreSQL, and REST APIs. Scripting unit and integration tests; leveraging Docker and DevOps for robust, scalable deployments.",
-        tools: ["TypeScript", "React", "Chakra UI", "Python", "Django", "Pandas", "MongoDB", "PostgreSQL", "REST APIs", "JSON Web Token"]
+        tools: ["TypeScript", "React", "Chakra UI", "Python", "Django", "Pandas", "MongoDB", "PostgreSQL", "REST APIs", "JSON Web Token", "ApexCharts.js", "Unit Testing", "Docker"]
     }, {
         name: "GroundBreakers Excavation",
         link: "https://groundbreakersexcavation.com/",
