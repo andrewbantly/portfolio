b412d9f2c795d66216947b4ad8f2b95506af6715
Andrew Bantly <andrewbantly@gmail.com>
2024-06-15 16:47:43 -0700

    project data structure update

--- components/Projects.js
+++ components/Projects.js
@@ -5,24 +5,36 @@ export default function Projects() {
 
     const projectList = myProjects.map((project, i) => {
         const techs = project.tools.map((tech, i) => {
-            return(
+            return (
                 <p key={`tech-key-${i}`} className={`${styles.technologies}`}>{tech}</p>
             )
         })
-        return(
-        <div className={`${styles.column}`} key={`key=${i}`}>
-            <a href={project.link} target="_blank"><h3 className={`${styles.projectHeader}`}>{project.name}</h3></a>
-            <div className={`${styles.techContainer}`}>
-            {techs}
-            </div>
-            <p className={`${styles.projectDescription}`}>{project.description}</p>
-        </div>
-        )
-    })
-            return(
-            <div className={`${styles.container}`}>
-                <h2>Projects</h2>
-                {projectList}
-            </div>
+        if (project.active) {
+            return (
+                <div className={`${styles.column}`} key={`key=${i}`}>
+                    <a href={project.link} target="_blank"><h3 className={`${styles.projectHeader}`}>{project.name}</h3></a>
+                    <div className={`${styles.techContainer}`}>
+                        {techs}
+                    </div>
+                    <p className={`${styles.projectDescription}`}>{project.description}</p>
+                </div>
             )
+        } else {
+            return (
+                <div className={`${styles.column}`} key={`key=${i}`}>
+                    <h3 className={`${styles.projectHeader}`}>{project.name}</h3>
+                    <div className={`${styles.techContainer}`}>
+                        {techs}
+                    </div>
+                    <p className={`${styles.projectDescription}`}>{project.description}</p>
+                </div>
+            )
+        }
+    })
+    return (
+        <div className={`${styles.container}`}>
+            <h2>Projects</h2>
+            {projectList}
+        </div>
+    )
 }
--- projectData.js
+++ projectData.js
@@ -3,38 +3,45 @@ const myProjects = [
         name: "SoBet",
         link: "https://sobet.io/",
         description: "As a Junior Developer at SoBet, I lead in the development and testing of new software, significantly accelerating feature rollouts and bug resolution timelines. My experience encompasses using PHP and Laravel for backend API solutions, with MySQL as the database of choice for all developments. On the front-end, I apply Vue.js and JavaScript to create intuitive user interfaces, while for mobile application development, I rely on TypeScript, React Native, and Redux for building robust solutions. Beyond the development work, I have authored comprehensive documentation to streamline the configuration of local environments, reducing setup time by 40%. Additionally, my efforts in enhancing user support have led to reductions in first reply and requester wait times by 1.1 and 1.2 hours, respectively.",
-        tools: ["PHP", "Laravel", "mySQL", "TypeScript", "React Native", "Redux", "Vue.js", "JavaScript"]
+        tools: ["PHP", "Laravel", "mySQL", "TypeScript", "React Native", "Redux", "Vue.js", "JavaScript"],
+        active: true,
     },
     {
         name: "LeasePeek (in progress)",
         link: "https://github.com/andrewbantly/leasepeek",
         description: "Engineering a data-driven app for large-scale property managers to forecast rent trends using analytics. Utilizing TypeScript, React, Chakra UI, Python, Django, Pandas, MongoDB, PostgreSQL, and REST APIs. Scripting unit and integration tests; leveraging Docker and DevOps for robust, scalable deployments.",
-        tools: ["TypeScript", "React", "Chakra UI", "Python", "Django", "Pandas", "MongoDB", "PostgreSQL", "REST APIs", "JSON Web Token", "ApexCharts.js", "Coverage.py", "Docker"]
+        tools: ["TypeScript", "React", "Chakra UI", "Python", "Django", "Pandas", "MongoDB", "PostgreSQL", "REST APIs", "JSON Web Token", "ApexCharts.js", "Coverage.py", "Docker"],
+        active: true,
     }, {
         name: "GroundBreakers Excavation",
         link: "https://groundbreakersexcavation.com/",
         description: "Built a family business website with SEO and accessibility using JavaScript, React, Chakra UI, and Netlify. Integrated serverless email alerts, boosting visibility, inclusivity, and client engagement.",
-        tools: ["JavaScript", "React", "Chakra UI", "Netlify"]
+        tools: ["JavaScript", "React", "Chakra UI", "Netlify"],
+        active: true,
     }, {
         name: "World Class Wines",
         link: "https://andrewbantly.github.io/world-class-wines/",
         description: "Programmed an interactive data visualization with JavaScript, React, D3.js, and Python, simplifying wine discovery by preferences, praised for user experience.",
-        tools: ["JavaScript", "D3.js", "React", "Python", "CSS"]
+        tools: ["JavaScript", "D3.js", "React", "Python", "CSS"],
+        active: true,
     }, {
-        name: "National Parks Explorer (retired)",
+        name: "National Parks Explorer",
         link: "https://nps-explorer.netlify.app/",
         description: "Co-developed a MERN stack app, enabling users to discover U.S. national parks and log visits, with features like REST APIs, JWT authentication, and CRUD operations.",
-        tools: ["Full-stack", "JavaScript", "CSS", "React", "Node.js","MongoDB", "Mongoose", "Express", "bcryptjs", "JSON Web Token", "REST APIs"]
+        tools: ["Full-stack", "JavaScript", "CSS", "React", "Node.js","MongoDB", "Mongoose", "Express", "bcryptjs", "JSON Web Token", "REST APIs"],
+        active: false,
     }, {
-        name: "Major League Baseball Pick'em (retired)",
+        name: "Major League Baseball Pick'em",
         link: "https://pickem.herokuapp.com/",
         description: "Crafted a full-stack Node app using JavaScript, featuring real-time game odds, social interactions, and performance metrics, supported by relational databases.",
-        tools: ["Full-stack", "JavaScript", "Node.js","PostgreSQL", "Sequelize", "EJS", "Express", "bcrypt", "crypto-js", "CSS"]
+        tools: ["Full-stack", "JavaScript", "Node.js","PostgreSQL", "Sequelize", "EJS", "Express", "bcrypt", "crypto-js", "CSS"],
+        active: false,
     }, {
         name: "Escape From Alcatraz",
         link: "https://andrewbantly.github.io/Escape-From-Alcatraz/",
         description: "Engineered an interactive game leveraging JavaScript for DOM manipulation, behavioral logic, and object detection, enhanced with intricate CSS styling.",
-        tools: ["JavaScript", "CSS", "HTML"]
+        tools: ["JavaScript", "CSS", "HTML"],
+        active: true,
     }
 ]
 
