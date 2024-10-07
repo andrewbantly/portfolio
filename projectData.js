d1771a524d8f0fb7c9ebd303413df57bf3e9876d
Andrew Bantly <andrewbantly@gmail.com>
2024-10-07 07:10:00 -0700

    leasepeek link

--- projectData.js
+++ projectData.js
@@ -12,7 +12,7 @@ const myProjects = [
         link: "https://github.com/andrewbantly/leasepeek",
         description: "Engineering a data-driven app for large-scale property managers to forecast rent trends using analytics. Utilizing TypeScript, React, Chakra UI, Python, Django, Pandas, MongoDB, PostgreSQL, and REST APIs. Scripting unit and integration tests; leveraging Docker for robust, scalable deployments.",
         tools: ["TypeScript", "React", "Chakra UI", "Python", "Django", "Pandas", "MongoDB", "PostgreSQL", "REST APIs", "JSON Web Token", "ApexCharts.js", "Coverage.py", "Docker"],
-        active: false,
+        active: true,
         deactivateDescription: 'in progress',
     }, {
         name: "GroundBreakers Excavation",
