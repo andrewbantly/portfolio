6bbb88427de2daa22a7a3e9114130ef5f150c2ff
Andrew Bantly <andrewbantly@gmail.com>
2025-04-18 16:53:20 -0700

    updated experience description

--- experienceData.js
+++ experienceData.js
@@ -5,8 +5,8 @@ const myExperience = [
         description: [
             "Supported over 20% growth in active users by delivering business-critical features and rapidly resolving user-reported bugs across mobile and web platforms",
             "Overhauled SoBet’s sports data architecture to support 70K+ athletes and 33K+ matchups across 20+ leagues; shipped a scalable schema, fast ingestion pipeline, and high-utility frontend—driving a 40.6% increase in visits, 149% longer time on page, and 6.3% boost in click-throughs",
+            "Pivotal in developing a robust bet tracking system handling 10K+ monthly bets across 100+ bettors; engineered automated reconciliation logic, enabled performance transparency with a dynamic frontend leaderboard, and built a reliable PHPUnit test suite",
             "Refactored mobile app state management, transitioning from Redux to Zustand to reduce complexity and improve stability, leading to a 58.5% drop in post-release BugSnag errors",
-            "Built a leaderboard system for 10K+ monthly bets across 100+ bettors, improving influencer transparency; engineered the ranking algorithm, shipped an intuitive frontend, and implemented comprehensive test coverage using PHPUnit",
             "Collaborated closely with product and design to scope features, break down work, and ship iteratively in a fast-paced environment"
         ],
         tools: [
